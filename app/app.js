a03d184511426f3070fd7df35ebc00785b935579
juanchycc <jcaserio@fi.uba.ar>
2022-09-23 17:48:12 -0300

    agregando sync y async

--- app/app.js
+++ app/app.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const request = require("request")
 
 const app = express();
 const PORT = 3000;
 
 app.get("/", (_req, _res) => {
-   _res.status(200).send("ping prueba");
+   _res.status(200).send("ping");
+});
+
+app.get("/sync", (_req, _res) => {
+    request("http://bbox:9090", {json:true}, (err, response, body) => {
+        if (err) {return console.log(err);}
+        _res.status(200).send(`Sync: ${response}`);
+    });
+});
+
+app.get("/async", (_req, _res) => {
+    request("http://bbox:9091", {json:true}, (err, response, body) => {
+        if (err) {return console.log(err);}
+        _res.status(200).send(`Async: ${response}`);
+    });
 });
 
 app.listen(PORT, () => {
