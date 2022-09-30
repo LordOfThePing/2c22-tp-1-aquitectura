7e42c5fc5b51f5a13c10e7f8cf0f28df4b218505
Franco <francopapa@gmail.com>
2022-09-30 10:34:00 -0300

    demo graficos python

---
+++ python_graphs/README.md
@@ -0,0 +1,9 @@
+**Para ejecutar**, corran artillery en la terminal redirigiendo el output a un archivo: 
+\$ npm run scenario <nombre_del_scenario> node >output.txt
+
+Luego modifiquen el nombre del archivo a abrir dentro de main.py
+
+Finalmente corran python3 main.py
+
+**Dependencias requeridas: numpy, matplotlib**
+Para instalar: pip3 install numpy; pip3 install matplotlib
---
+++ python_graphs/main.py
@@ -0,0 +1,51 @@
+from email import iterators
+from urllib import response
+import numpy as np 
+from matplotlib import pyplot
+
+def main():
+    with open("endurance.txt", "r") as file:
+        lines = file.readlines()
+
+    p95_response_time(lines)
+
+def p95_response_time(lines):
+    p95 = parse_p95_resp_time(lines) 
+    time = []
+
+    # Each measurement is from a 10 second interval
+    for i in range(len(p95)):
+        time.append((i+1) * 10)
+    
+    pyplot.plot(time, p95)
+    pyplot.xlabel("Time (s)")
+    pyplot.ylabel("Response Time (ms) ")
+    pyplot.show()
+
+def parse_p95_resp_time(lines):
+    belongs_to_reponse_time = False
+    
+    response_time = []
+
+    for line in lines:
+        if "http.response_time" in line:
+            belongs_to_reponse_time = True
+        if "session_length" in line:
+            belongs_to_reponse_time = False
+        if "p95" in line and belongs_to_reponse_time:
+            number = line.split(" ")[-1].replace("\n", "")
+            response_time.append(number)
+            belongs_to_reponse_time = False
+    return response_time
+
+#Matches the length of n arrays to the minimum of them by popping
+#elements from longer arrays
+def trunc_longer_arrays(arrays):
+    lengths = [len(arr) for arr in arrays]
+    min_length = np.array(lengths).min()    
+
+    for i in range(len(arrays)):
+        for j in range(min_length, len(arrays[i])):
+            arrays[i].pop()
+
+#main()
