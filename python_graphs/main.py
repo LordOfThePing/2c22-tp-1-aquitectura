8e486858569568cddc6a1b7b748a00ce3990c3b0
LordOfThePing <pepe_flynn@hotmail.com>
2022-09-30 17:19:48 -0300

    collab

--- python_graphs/main.py
+++ python_graphs/main.py
@@ -1,14 +1,43 @@
+import base64
+import requests
+
+def req(link):
+  master = "https://raw.githubusercontent.com/LordOfThePing/2c22-tp-1-aquitectura/graficos-python/python_graphs/" + link
+  return requests.get(master).text
+
+ping_breakpoint = req("ping/breakpoint.txt")
+ping_stress = req("ping/stress.txt")
+ping_endurance = req("ping/endurance.txt")
+
 from email import iterators
 from urllib import response
 import numpy as np 
 from matplotlib import pyplot
 
-def main(filename, params, i):
-    with open(filename, "r") as file:
-        lines = file.readlines()
+pyplot.rcParams['figure.figsize'] = [15, 5]
 
-    
-    i(lines, params)
+
+
+
+#def main(filename, params, i):
+    #with open(filename, "r") as file:
+    #    lines = file.readlines()
+
+def main(strfile, title, graphs):
+
+
+    i = 0
+    #pyplot.figure(i)
+    pyplot.title(title)
+    for graph in graphs:
+      i = i+1
+      pyplot.subplot(1,2, i)
+      lines = iter(strfile.splitlines())
+
+      graph[0](lines, graph)
+
+    pyplot.show()
+
 
 def p95_response_time(lines, params):
     p95 = parse_p95_resp_time(lines) 
@@ -19,13 +48,11 @@ def p95_response_time(lines, params):
         time.append(i * 10)
     p95.insert(0, 0)
 
-
     pyplot.plot(time, p95)
     pyplot.grid()   
     pyplot.xlabel(params[2])
     pyplot.ylabel(params[1])
-    pyplot.title(params[0])
-    pyplot.show()
+
 
 def plot_number_of_requests(lines, params):
     reqs = parse_num_of_reqs(lines) 
@@ -35,13 +62,11 @@ def plot_number_of_requests(lines, params):
         time.append(i * 10)
     reqs.insert(0, 0)
 
-
     pyplot.plot(time, reqs)
     pyplot.grid()   
     pyplot.xlabel(params[2])
     pyplot.ylabel(params[1])
-    pyplot.title(params[0])
-    pyplot.show()
+
 
 
 def parse_p95_resp_time(lines):
@@ -73,6 +98,7 @@ def parse_num_of_reqs(lines):
         if "Summary report" in line:
             break
     return requests
+    
 #Matches the length of n arrays to the minimum of them by popping
 #elements from longer arrays
 def trunc_longer_arrays(arrays):
